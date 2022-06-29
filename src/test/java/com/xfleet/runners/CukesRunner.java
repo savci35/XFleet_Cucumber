d7c03b0e9d245a9ea9f2910dfe5e3d6c8bacbe45
savci35 <ebrusavas2525@gmail.com>
2022-06-29 17:04:26 +0300

    Framework is ready for the features

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -1,5 +1,25 @@
 package com.xfleet.runners;
 
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/reports/cucumber-report.html",
+                "json:target/reports/cucumber-report.json",
+                "rerun:target/reports/cucumber-report.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features", // copy and chose content root
+        glue = "com/xfleet/step_definitions",   // sağ klik stepDefinition ve  source root
+        dryRun = false,
+        tags = "",
+        publish = false
+
+)
 public class CukesRunner {
 }
--- src/test/java/com/xfleet/utilities/BrowserUtils.java
+++ src/test/java/com/xfleet/utilities/BrowserUtils.java
@@ -1,4 +1,14 @@
 package com.xfleet.utilities;
 
 public class BrowserUtils {
+
+
+    public static void sleep(int seconds)  {
+
+        try {
+            Thread.sleep(seconds*1000);
+        }catch(InterruptedException e){
+            System.out.println("static sleep issue");
+        }
+    }
 }
